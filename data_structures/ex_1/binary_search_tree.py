5b66080c36fcd2730a5fa0de71a1d9d90def0e27
Grant Reighard <grant.reighard@gmail.com>
2018-10-12 12:25:42 -0400

    Got tests passing in BST.

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -1,26 +1,56 @@
+import sys
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
     self.left = None
     self.right = None
+    self.children = []
+
+  def get_rev_children(self):
+    children = self.children[:]
+    children.reverse()
+    return children
+
+  def get_children(self):
+    return self.children
 
   def depth_first_for_each(self, cb):
-    pass 
-    # hello   
+    nodes = []
+    stack = [self]
+    while stack:
+      curr_node = stack[0]
+      stack = stack[1:]
+      nodes.append(curr_node)
+      cb(curr_node.value)
+      for child in curr_node.get_rev_children():
+        stack.insert(0, child)
+    return nodes
 
   def breadth_first_for_each(self, cb):
-    pass
+    nodes = []
+    stack = [self]
+    while stack:
+      curr_node = stack[0]
+      stack = stack[1:]
+      nodes.append(curr_node)
+      cb(curr_node.value)
+      for child in curr_node.get_children():
+        stack.append(child)
+    return nodes
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
     if (value < self.value):
       if not self.left:
         self.left = new_tree
+        self.children.append(new_tree)
       else:
         self.left.insert(value)
     elif value >= self.value:
       if not self.right:
         self.right = new_tree
+        self.children.append(new_tree)
       else:
         self.right.insert(value)
 
